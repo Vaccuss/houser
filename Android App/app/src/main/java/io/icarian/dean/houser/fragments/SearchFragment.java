92aff8194dc8408a6f4396ff869fb819797e9a78
Vaccuss <Deansteven308@gmail.com>
2016-07-21 17:31:07 +1000

    directory changes

--- Android App/app/src/main/java/io/icarian/dean/houser/fragments/SearchFragment.java
+++ Android App/app/src/main/java/io/icarian/dean/houser/fragments/SearchFragment.java
@@ -5,41 +5,38 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 import io.icarian.dean.houser.R;
 import io.icarian.dean.houser.utils.Requester;
 
+
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link DashboardFragment.OnFragmentInteractionListener} interface
+ * {@link SearchFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link DashboardFragment#newInstance} factory method to
+ * Use the {@link SearchFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DashboardFragment extends Fragment {
+public class SearchFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -51,8 +48,7 @@ public class DashboardFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-
-    public DashboardFragment() {
+    public SearchFragment() {
         // Required empty public constructor
     }
 
@@ -62,11 +58,11 @@ public class DashboardFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
+     * @return A new instance of fragment SearchFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
+    public static SearchFragment newInstance(String param1, String param2) {
+        SearchFragment fragment = new SearchFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -77,32 +73,22 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-
-
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
 
-        final TextView mTxtDisplay;
-        mTxtDisplay = (TextView) view.findViewById(R.id.contentText);
 
 
-        Button getJsonButtton = (Button) view.findViewById(R.id.json_clicker);
-        getJsonButtton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                getData(mTxtDisplay);
-            }
-        });
 
-        // Inflate the layout for this fragment
         return view;
     }
 
@@ -145,9 +131,11 @@ public class DashboardFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
+
     final void getData(final TextView display) {
         Log.i("getData", "Called");
         final String url = "http://192.168.99.1:3000/api";
+
 
         JsonArrayRequest jsObjRequest = new JsonArrayRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
@@ -188,6 +176,4 @@ public class DashboardFragment extends Fragment {
         Log.i("getData", "Ended");
 
     }
-
-
 }
